0c80feab2d477078b3948889cf67a78fa5ed00c1
Adam Wulf <adam.wulf@gmail.com>
2013-10-16 23:36:42 -0400

    comments

--- config-sample.php
+++ config-sample.php
@@ -1,5 +1,8 @@
 <?
 
+// instructions for finding your API username and password are here:
+// https://github.com/adamwulf/automatic-php-api/wiki
+
 define("USERNAME", "API username (not Automatic account username)");
 define("PASSWORD", "API password (not Automatic account password)");
 
