43b1997f47dd6041506925d415fe570b012dc2f6
Adam Wulf <adam.wulf@gmail.com>
2014-01-16 02:56:14 -0500

    udpated sample config

--- config-sample.php
+++ config-sample.php
@@ -2,11 +2,11 @@
 
 define("AUTOMATIC_CLIENT_ID", "Automatic Client ID");
 define("AUTOMATIC_CLIENT_SECRET", "Automatic Client Secret");
-define("REDIRECT_URI", "http://your.url.to/redirect/to");
+define("AUTOMATIC_REDIRECT_URI", "http://your.url.to/redirect/to");
 
 
-define("AUTHORIZATION_ENDPOINT", "https://www.automatic.com/oauth/authorize");
-define("TOKEN_ENDPOINT", "https://www.automatic.com/oauth/access_token");
-define("API_ENDPOINT", "https://api.automatic.com/v1/");
+define("AUTOMATIC_AUTHORIZATION_ENDPOINT", "https://www.automatic.com/oauth/authorize");
+define("AUTOMATIC_TOKEN_ENDPOINT", "https://www.automatic.com/oauth/access_token");
+define("AUTOMATIC_API_ENDPOINT", "https://api.automatic.com/v1/");
 
 ?>
