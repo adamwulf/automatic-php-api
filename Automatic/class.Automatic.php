d66759a81abada375f8f981ea0f1bdbdb1709547
Adam Wulf <adam.wulf@gmail.com>
2014-01-16 01:31:27 -0500

    added oauth2 library. refactored the Automatic class to work with the new official API.

--- Automatic/class.Automatic.php
+++ Automatic/class.Automatic.php
@@ -4,157 +4,65 @@
 
 class Automatic{
 
-	protected $username;
-	protected $password;
+	// the OAuth2 client
+	private $client;
+	
+	private $token;
 
-	/**
-	 * for instructions on how to find your username
-	 * and password for the Automatic API, view
-	 * the instructions in the README
-	 */
-	public function __construct($username, $password){
-		$this->username = $username;
-		$this->password = $password;
-	}
-
-	/**
-	 * This method will return preference and account
-	 * information, including name, timezone, audio
-	 * preferences, vin numbers, etc
-	 */	
-	public function getPreferences(){
-		return $this->getRequest("/v3/user/preferences");
+	public function __construct($client_id, $client_secret){
+		$this->client = new OAuth2\Client($client_id, $client_secret);
+	    $this->client->setAccessTokenType(OAuth2\Client::ACCESS_TOKEN_TOKEN);
 	}
 	
-	/**
-	 * This method will return information about
-	 * the Automatic links on your account.
-	 */
-	public function getLinkInfo(){
-		return $this->getRequest("/v3/link/info");
+	public function getOAuthToken(){
+		return $this->token;
 	}
 	
-	/**
-	 * This will return detailed information about
-	 * the cars in your account
-	 */
-	public function getCars(){
-		return $this->getRequest("/v3/car");
+	public function setOAuthToken($token){
+		$this->token = $token;
+		$this->client->setAccessToken($token);
 	}
 	
-	/**
-	 * this will update the audio notifications in the server,
-	 * however, the iPhone app seems to also store its audio
-	 * settings locally. After the iPhone checks in to the server,
-	 * even though it gets the values that you've set here, it'll
-	 * reset it to whatever was set on the phone, overriding
-	 * whatever you set here.
-	 *
-	 * this method is kept in this API in case the iPhone app ever
-	 * starts to respect the values sent back from the server instead
-	 * of overriding it with its local values.
-	 */
-	public function updateAudioNotifications($brake, $speed, $accel){
-		$data = array("brake" => $brake, "speed_warn" => $speed, "accel" => $accel);
-		$data = array("audioSettings" => $data);
-		$data_string = json_encode($data);
-		$data_string = http_build_query(array("data" => $data_string));
-		return $this->postRequest("/v3/user/preferences", $data_string);
-	}	
-	
-	/**
-	 * returns all trips that are between the start and end
-	 * timestamps.
-	 *
-	 * input timestamps in milliseconds
-	 */
-	public function getTrips($start_time, $end_time){
-		$data = array("end_time" => $end_time, "start_time" => $start_time);
-		$data_string = json_encode($data);
-		$data_string = http_build_query(array("data" => $data_string));
-		return $this->postRequest("/v3/user/trips/", $data_string);
+	public function isLoggedIn(){
+		return strlen($this->token);
 	}
 	
-	/**
-	 * returns an array of weekly scores that fall between the input
-	 * start and end timestamps
-	 *
-	 * input timestamps in milliseconds
-	 */
-	public function getScores($start_time, $end_time){
-		$data = array("end_time" => $end_time, "start_time" => $start_time);
-		$data_string = json_encode($data);
-		$data_string = http_build_query(array("data" => $data_string));
-		return $this->postRequest("/v3/user/scores/", $data_string);
-	}
-	
-	/**
-	 * returns the current parked location for the vehicle
-	 * of the input vin number
-	 */
-	public function getParkedLocations($vin){
-		$data = array("vin" => $vin);
-		$data_string = json_encode($data);
-		$data_string = http_build_query(array("data" => $data_string));
-		return $this->postRequest("/v3/car/parked_location/", $data_string);
-	}
-
-
-	/**
-	 * helper method to send a GET request
-	 * to the API
-	 */	
-	protected function getRequest($endpoint){
-		$host = "https://secure.milesense.com" . $endpoint;
-	
-		$opts = array(
-		  'http'=>array(
-		    'method'=>"GET",
-		    'header'=>"Authorization: Basic ".base64_encode($this->username . ":" . $this->password)."\r\n"
-		  )
-		);
-		
-		$context = stream_context_create($opts);
-		$fp = fopen($host, 'r', false, $context);
-		$contents = stream_get_contents($fp);
-		// read the content from $fp here.
-		fclose($fp);
-		
-		if($contents){
-			$contents = json_decode($contents);
+	public function authenticationURLForScopes($scopes){
+		if(!is_array($scopes)){
+			throw new InvalidArgumentException("\$scopes parameter to authenticateForScopes should be an array");
 		}
-		
-		return $contents;
+		$scopes = implode(" ", $scopes);
+	    return $this->client->getAuthenticationUrl(AUTOMATIC_AUTHORIZATION_ENDPOINT, AUTOMATIC_REDIRECT_URI, array("scope" => $scopes));
+	}
+	
+	public function getTokenForCode($code){
+	    $params = array('code' => $_GET['code'], 'redirect_uri' => AUTOMATIC_REDIRECT_URI);
+	    $response = $this->client->getAccessToken(AUTOMATIC_TOKEN_ENDPOINT, 'authorization_code', $params);
+	    $this->client->setAccessToken($response['result']['access_token']);
+	    return $response['result']['access_token'];
 	}
 	
 	/**
-	 * helper method to send a POST request
-	 * to the API
-	 */	
-	protected function postRequest($endpoint, $data_string){
-		$host = "https://secure.milesense.com" . $endpoint;
-	
-		$opts = array(
-		  'http'=>array(
-		    'method'=>"POST",
-		    'header'=>"Authorization: Basic ".base64_encode($this->username . ":" . $this->password)."\r\n"
-             . "Content-Length: " . strlen($data_string) . "\r\n"
-             . "Content-Type: application/x-www-form-urlencoded\r\n",
-			'content' => $data_string
-		  )
-		);
-		
-		$context = stream_context_create($opts);
-		$fp = fopen($host, 'r', false, $context);
-		$contents = stream_get_contents($fp);
-		// read the content from $fp here.
-		fclose($fp);
-		
-		if($contents){
-			$contents = json_decode($contents);
-		}
-		
-		return $contents;
+	 * returns up to $per_page trips starting with
+	 * the most recent trip after $page * $per_page trips
+	 *
+	 * @param $page the page of trips to start on (begins at 1)
+	 * @param $per_page the number of trips to return per page
+	 */
+	public function getTrips($page=1, $per_page=100){
+		$parameters = array("page" => $page, "per_page" => $per_page);
+		$params = http_build_query($parameters, null, '&');
+		return $this->client->fetch(AUTOMATIC_API_ENDPOINT . "trips?" . $params);
 	}
 	
+	
+	/**
+	 * returns a single trip specified by the input id
+	 *
+	 * @param $trip_id the id of the trip to fetch
+	 */
+	public function getTrip($trip_id){
+		return $this->client->fetch(AUTOMATIC_API_ENDPOINT . "trip/" . urlencode($trip_id));
+	}
+
 }
--- config-sample.php
+++ config-sample.php
@@ -1,9 +1,12 @@
 <?
 
-// instructions for finding your API username and password are here:
-// https://github.com/adamwulf/automatic-php-api/wiki
+define("AUTOMATIC_CLIENT_ID", "Automatic Client ID");
+define("AUTOMATIC_CLIENT_SECRET", "Automatic Client Secret");
+define("REDIRECT_URI", "http://your.url.to/redirect/to");
 
-define("USERNAME", "API username (not Automatic account username)");
-define("PASSWORD", "API password (not Automatic account password)");
+
+define("AUTHORIZATION_ENDPOINT", "https://www.automatic.com/oauth/authorize");
+define("TOKEN_ENDPOINT", "https://www.automatic.com/oauth/access_token");
+define("API_ENDPOINT", "https://api.automatic.com/v1/");
 
 ?>
