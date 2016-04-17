c84b4fa6f1c51de206f293a3a0e8de3fabe6db0c
Ethan Henderson <ethan@zbee.me>
2016-04-17 12:57:25 -0600

    Update config-sample.php
    Remove redirect uri variable, update endpoint urls

--- config-sample.php
+++ config-sample.php
@@ -1,12 +1,10 @@
-<?
+<?php
 
 define("AUTOMATIC_CLIENT_ID", "Automatic Client ID");
 define("AUTOMATIC_CLIENT_SECRET", "Automatic Client Secret");
-define("AUTOMATIC_REDIRECT_URI", "http://your.url.to/redirect/to");
 
-
-define("AUTOMATIC_AUTHORIZATION_ENDPOINT", "https://www.automatic.com/oauth/authorize");
-define("AUTOMATIC_TOKEN_ENDPOINT", "https://www.automatic.com/oauth/access_token");
-define("AUTOMATIC_API_ENDPOINT", "https://api.automatic.com/v1/");
+define("AUTOMATIC_AUTHORIZATION_ENDPOINT", "https://accounts.automatic.com/oauth/authorize");
+define("AUTOMATIC_TOKEN_ENDPOINT", "https://accounts.automatic.com/oauth/access_token");
+define("AUTOMATIC_API_ENDPOINT", "https://api.automatic.com/");
 
 ?>
